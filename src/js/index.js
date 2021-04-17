9fe9f240ab4c5a886baf5777926a9c230035b7d2
M.Urazbakhtin <M.Urazbakhtin@russianpost.ru>
2021-04-17 20:25:36 +0300

    complete template for web (desktop) version

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,17 @@ import CSSKeyframer from 'css-keyframer'
 import {initFormField} from "./form-field-module";
 console.log('Hello World from index main file!')
 initFormField('contact-name', 'Enter your name')
+initFormField('contact-email', 'Enter your email')
+initFormField('contact-message', 'Write your message')
+document.addEventListener('readystatechange', (event) => {
+    if(document.readyState === 'complete'){
+            document.getElementsByClassName('contact__form-fields')[0].style.cssText = 'margin-left:0px !important'
+            const formFields = document.getElementsByClassName('contact__form-field')
+            for(let i = 0;i < formFields.length;i++){
+                formFields[i].style.cssText = 'margin-left:0px !important'
+            }
+
+    }
+});
 
 
